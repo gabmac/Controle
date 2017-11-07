de57ba2abd80f78c8e7e45011ab3dd9dc0d6b135
gabmac <30659678+gabmac@users.noreply.github.com>
2017-11-07 00:29:12 -0200

    Update computacional1.m

--- computacional1.m
+++ computacional1.m
@@ -2,6 +2,8 @@ clear all;
 close all;
 clc;
 
+
+%valores de cada variavel
 m = 1;
 b = 1.725;
 k = 0.575;
@@ -9,6 +11,7 @@ j = 0.15;
 l = 1;
 L = 2;
 
+%valores para funcao transferencia
 M = m*l^2+j;
 C = b*l^2;
 K = k*l^2;
@@ -17,6 +20,8 @@ s = tf('s');
 
 G = L/(M*s^2+C*s+K);
 
+
+%valores advindos do step
 A1 = pi*K/(L*9);
 
 A2 = pi*K/(L*3);
@@ -25,11 +30,15 @@ step(A1*G);
 hold on;
 step(A2*G);
 
+
+%valor do Wn
 Wn = ((k*l^2)/M)^0.5;
 
 doisWncsi = C/M;
+
+
 csi = doisWncsi/(2*Wn);
 
-
+%tempo de estabilização
 te = 4/(doisWncsi/2);
 
